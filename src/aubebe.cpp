b0a82ef38ff24cfbf17784415528a8545c409e50
Conrad Parker <conrad@metadecks.org>
2024-03-17 07:33:45 +1100

    Add file selector button, print filename

--- src/aubebe.cpp
+++ src/aubebe.cpp
@@ -1,6 +1,31 @@
 #include "aubebe.h"
 
+#include <iostream>
+
 #include "imgui.h"
+#include "ImGuiFileDialog.h"
+
+void filedialog() {
+  // open Dialog Simple
+  if (ImGui::Button("Open File Dialog")) {
+    IGFD::FileDialogConfig config;
+	config.path = ".";
+    ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Choose File", ".cpp,.h,.hpp", config);
+  }
+  // display
+  if (ImGuiFileDialog::Instance()->Display("ChooseFileDlgKey")) {
+    if (ImGuiFileDialog::Instance()->IsOk()) { // action if OK
+      std::string filePathName = ImGuiFileDialog::Instance()->GetFilePathName();
+      std::string filePath = ImGuiFileDialog::Instance()->GetCurrentPath();
+      // action
+      std::cout << "filePathName: " << filePathName << std::endl;
+      std::cout << "filePath: " << filePath << std::endl;
+    }
+
+    // close
+    ImGuiFileDialog::Instance()->Close();
+  }
+}
 
 void AuBeBe::ShowMainWindow(bool* p_open)
 {
@@ -8,5 +33,8 @@ void AuBeBe::ShowMainWindow(bool* p_open)
     ImGui::Text("Hello from aubebe!");
     if (ImGui::Button("Close Me"))
         *p_open = false;
+
+    filedialog();
+
     ImGui::End();
 }
